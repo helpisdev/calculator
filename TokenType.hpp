103908e02f600a3801f28598dca8e1f24e7e0b74
iliaskap12 <iliaskapareliotis@gmail.com>
2021-10-24 12:25:46 +0300

    Implement TokenType

---
+++ TokenType.hpp
@@ -0,0 +1,14 @@
+//
+// Created by ilias on 22/10/21.
+//
+
+#ifndef CALCULATOR_TOKENTYPE_HPP
+#define CALCULATOR_TOKENTYPE_HPP
+
+enum class TokenType
+{
+    OPERATOR,
+    NUMBER
+};
+
+#endif //CALCULATOR_TOKENTYPE_HPP
