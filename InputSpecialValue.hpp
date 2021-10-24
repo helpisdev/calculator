af7283016e853dd8d7646f6233f88dc238497af1
iliaskap12 <iliaskapareliotis@gmail.com>
2021-10-24 12:26:16 +0300

    Implement InputSpecialValue

---
+++ InputSpecialValue.hpp
@@ -0,0 +1,16 @@
+//
+// Created by ilias on 23/10/21.
+//
+
+#ifndef CALCULATOR_INPUTSPECIALVALUE_HPP
+#define CALCULATOR_INPUTSPECIALVALUE_HPP
+
+enum class InputSpecialValue : char
+{
+    EQUALS_PROMT = '=',
+    ENTER_PROMT = '\n',
+    SPACE = ' ',
+    EXIT = 'q'
+};
+
+#endif //CALCULATOR_INPUTSPECIALVALUE_HPP
