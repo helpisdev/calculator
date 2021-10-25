36b7d74dbe02086361dcc35c32aae7d6c87417e1
iliaskap12 <iliaskapareliotis@gmail.com>
2021-10-25 21:53:13 +0300

    Extract Operator types as separate enum

---
+++ Operator.hpp
@@ -0,0 +1,19 @@
+//
+// Created by ilias on 24/10/21.
+//
+
+#ifndef CALCULATOR_OPERATOR_HPP
+#define CALCULATOR_OPERATOR_HPP
+
+enum class Operator : char
+{
+    ADDITION = '+',
+    SUBTRACTION = '-',
+    MULTIPLICATION = '*',
+    DIVISION = '/',
+    POWER = '^',
+    LEFT_PARENTHESIS = '(',
+    RIGHT_PARENTHESIS = ')'
+};
+
+#endif //CALCULATOR_OPERATOR_HPP
